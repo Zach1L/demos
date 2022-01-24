2ca24e69472b2b9e4223f9f48634edb2b429ef36
Zach Levine <Zach1Levine@gmail.com>
2022-01-23 18:03:43 -0800

    process multiple lines on same bet

--- betting/process_data.py
+++ betting/process_data.py
@@ -14,31 +14,34 @@ def process_betting_data(json_data):
 
 
     # Group bet ID
-    data['same_bet'] = data['id'].apply(lambda x:''.join(re.split('P|N|O|U', x.split('_')[-2])))
+    data['same_bet'] = data['offers.id']
     # Mathematically Sensible Odds
     data['odd_math'] = data['oddsDecimal'] - 1.0
     # Seed Fields
+    data['min_boost_to_positive'] = np.nan
+    data['free_bet_profit_pct'] = np.nan
     data['bet1'] = 100.0 
     data['bet2_30boost_on_pos'] = np.nan
     data['winnings1'] = np.nan
     data['winnings2'] = np.nan
     data['good_bet'] = False
-    data['min_boost_to_positive'] = np.nan
-    data['min_boost_to_negitive'] = np.nan
 
 
-    # https://www.wolframalpha.com/input/?i=%28a%2Bx%29+%2F+%28a%2B1%2Bx%29+%3D+1+%2F%28b%2B1%29++for+x
     for bet in data['same_bet'].unique():
         mask = data['same_bet'] == bet
-        c = data[mask]
-        if len(c) ==2:
-            data.loc[mask,'min_boost_to_positive'] = 1/c['odd_math'].min() - c['odd_math'].max()
-            data.loc[mask,'min_boost_to_negitive'] = 1/c['odd_math'].max() - c['odd_math'].min()
-
-            data.loc[mask,'bet2_30boost_on_pos'] = c['bet1'].mean() * (c['odd_math'].max() + 1 + 0.30) / (c['odd_math'].min() + 1)
-            data.loc[mask,'winnings1'] =  c['bet1'].mean() + c['bet1'].mean() * (c['odd_math'].max() + 0.30)
-            data.loc[mask,'winnings2'] =  data.loc[mask,'bet2_30boost_on_pos'] + data.loc[mask,'bet2_30boost_on_pos'] * (c['odd_math'].min())
-
+        c_big = data[mask]
+        if len(c_big) % 2 == 0:
+            pass
+            for i in  range(2,len(c_big)+2,2):
+                c = c_big[i-2:i]
+                data.loc[c.index,'min_boost_to_positive'] = 1/(c['odd_math'].min() * c['odd_math'].max())
+                data.loc[c.index, 'free_bet_profit_pct'] = c['odd_math'].max() - c['odd_math'].max() / (c['odd_math'].min() + 1)
+            if False:
+                boosted_o1 = c['odd_math'].max() * 1.3
+                odds_ratio = (boosted_o1 + 1) / (c['odd_math'].min() + 1)
+                data.loc[c.index,'bet2_30boost_on_pos'] = c['bet1'].mean() * odds_ratio
+                data.loc[c.index,'winnings1'] =  c['bet1'].mean() + c['bet1'].mean() * boosted_o1
+                data.loc[c.index,'winnings2'] =  data.loc[c.index,'bet2_30boost_on_pos'] + data.loc[c.index,'bet2_30boost_on_pos'] * (c['odd_math'].min())
     return data
 
 
